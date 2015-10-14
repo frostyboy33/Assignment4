ea8a0f09ed05029fda6bdd59efc3d485b636888a
Jaan Liiband <jaan@liiband.com>
2015-10-14 12:01:29 +1100

    Created a small helper function search for random values.

--- src/src/bugtesting/bug03/TestDiveValue.java
+++ src/src/bugtesting/bug03/TestDiveValue.java
@@ -10,6 +10,8 @@ import src.DiceValue;
 
 public class TestDiveValue {
 
+    private final Integer NUMBER_OF_TESTS = 10000;
+    
     @Before
     public void setUp() throws Exception {
     }
@@ -21,10 +23,48 @@ public class TestDiveValue {
     }
 
 
-
+    CROWN, ANCHOR, HEART, DIAMOND, CLUB, SPADE
     @Test
-    public void testGetRandom() {
+    public void testGetRandomGetCrown() {
         fail("Not yet implemented");
+    }
+    
+    
+    @Test
+    public void testGetRandomGetAnchor() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testGetRandomGetHeart() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testGetRandomGetDiamond() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testGetRandomGetClub() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testGetRandomGetSpade() {
+        fail("Not yet implemented");
+    }
+    
+    private boolean canProduceValue(DiceValue value) {
+        boolean found = false;
+        for (int i = 0; i < this.NUMBER_OF_TESTS; i++) {
+            DiceValue randomValue = DiceValue.getRandom();
+            if(randomValue == value) {
+                found = true;
+                i = this.NUMBER_OF_TESTS;
+            }
+        }
+        return found;
     }
 
 }
