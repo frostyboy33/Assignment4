41ca91f87863e6b0a208b028bce728a785a2749e
Jaan Liiband <jaan@liiband.com>
2015-10-14 11:53:22 +1100

    setup of the initial Junit class to test DiceValue

---
+++ src/src/bugtesting/bug03/TestDiveValue.java
@@ -0,0 +1,30 @@
+package src.bugtesting.bug03;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import src.DiceValue;
+
+public class TestDiveValue {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+
+    @Test
+    public void testGetRandom() {
+        fail("Not yet implemented");
+    }
+
+}
